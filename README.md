d5ca6097dd1cd5266f1b198142e3a251dd6c195a
Young <young.l@so-in.com>
2025-09-16 16:27:14 +0800

    refactor: 部署至cloudflare

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Personal Technical Homepage
+# Personal Homepage
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import Avatar from "@/components/Avatar";
-import ICP from "@/components/ICP";
 import Nav from "@/components/Nav";
 import Social from "@/components/Social";
 
 function App() {
 	return (
-		<div className="h-screen overflow-y-auto">
-			<div className="h-full container min-w-[400px] mx-auto grid grid-rows-[1fr_1fr_50px] place-items-center">
-				<section className="flex flex-col gap-10">
-					<Avatar />
-					<Social />
-				</section>
-				<Nav />
-				<ICP />
-			</div>
+		<div className="h-screen overflow-y-auto container min-w-[400px] mx-auto grid grid-rows-[1fr_1fr] place-items-center">
+			<section className="flex flex-col gap-10">
+				<Avatar />
+				<Social />
+			</section>
+			<Nav />
 		</div>
 	);
 }
